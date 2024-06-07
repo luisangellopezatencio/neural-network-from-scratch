d86d3269791edf2f902d68761cee56cb14ea3f6e
Luis A Lopez Atencio <luisangellopezatencio@outlook.com>
2024-06-07 08:03:52 -0500

    Created function to init the perceptrons

--- nn.py
+++ nn.py
@@ -15,11 +15,22 @@ class Perceptron:
     def set_weights(self, w):
         self.w = w
     
-class MLP(Perceptron):
-    def __init__(self, num_inputs: int, hidden_layes: tuple, num_outputs: int, sigma: float = 0.01) -> None:
-        pass
+class MLP():
+    def __init__(self, num_inputs: int, hidden_layers: tuple, num_outputs: int, sigma: float = 0.01) -> None:
+        self.all_perceptrons = []
+        inputs_layer = self.init_perceptrons(num_inputs, num_outputs, sigma)
+        self.all_perceptrons.append(inputs_layer)
+        for num_perceptrons in hidden_layers:
+            self.all_perceptrons.append(self.init_perceptrons(num_perceptrons, num_outputs, sigma))
     
     def feed_forward(self, X):
         y_hat = self.p3.predict(self.p2.predict(self.p1.predict(X)))
         return y_hat
+    
+    def init_perceptrons(self, num_perceptrons:int, num_outputs:int, sigma:float=0.01) -> list[Perceptron]:
+        perceptrons = []
+        for _ in range(num_perceptrons):
+            p = Perceptron(num_perceptrons, num_outputs, sigma)
+            perceptrons.append(p)
+        return perceptrons
         
