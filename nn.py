110b394886ab353dcb519ed244ff7e1e1de312c8
Luis A Lopez Atencio <luisangellopezatencio@outlook.com>
2024-06-06 19:40:18 -0500

    Added methods to set and get the weights

--- nn.py
+++ nn.py
@@ -1,13 +1,19 @@
 import numpy as np
 
 class Perceptron:
-    def __init__(self, num_inputs: int, sigma: float = 0.01) -> None:
-        self.w = np.random.normal(0, sigma, (num_inputs + 1 , 1)) #+1 to include bias
+    def __init__(self, num_inputs: int, num_outputs: int, sigma: float = 0.01) -> None:
+        self.w = np.random.normal(0, sigma, (num_inputs + 1 , num_outputs)) #+1 to include bias
     
     def predict(self, X):
         X = np.hstack((np.ones((X.shape[0], 1)), X))
         y_hat = np.matmul(X, self.w)
         return y_hat
+    
+    def get_weights(self):
+        return self.w
+
+    def set_weights(self, w):
+        self.w = w
     
 class MLP(Perceptron):
     def __init__(self, num_inputs: int, hidden_layes: tuple, num_outputs: int, sigma: float = 0.01) -> None:
